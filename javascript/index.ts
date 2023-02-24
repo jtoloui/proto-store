1e9147f5678c93e6d00e914966b05d4e702c9ce5
Jamie Toloui <jamie@toloui.com>
2023-02-24 21:19:49 +0000

    bump v1.0.2

--- javascript/index.ts
+++ javascript/index.ts
@@ -1,3 +1,2 @@
-export * from "./movies/v1/movie_pb";
-export * from "./movies/v1/movie_pb.client";
+export * from "./movies/v1/movie_pb.grpc-client";
 export * from "./movies/v1/movie_pb.grpc-server";
