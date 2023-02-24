d5c6a0f364619bc3b1e20058e10cf88ad547e4a7
Jamie Toloui <jamie@toloui.com>
2023-02-24 10:22:14 +0000

    set single plugin for server & client side ts and opts for naming of files

--- javascript/index.ts
+++ javascript/index.ts
@@ -1,2 +1,3 @@
-export * from "./movies/v1/movie";
-export * from "./movies/v1/movie.client";
+export * from "./movies/v1/movie_pb";
+export * from "./movies/v1/movie_pb.client";
+export * from "./movies/v1/movie_pb.grpc-server";
